1be840c7e85308c6fa42a3b0c8293413f233f23b
Kiran-Shabu <110695065+Kiran-Shabu@users.noreply.github.com>
2025-05-14 18:40:53 +0530

    Initial comiit - One to  one chat app

---
+++ client/src/assets/assets.js
@@ -0,0 +1,32 @@
+import avatar_icon from './avatar_icon.png'
+import gallery_icon from './gallery_icon.svg'
+import help_icon from './help_icon.png'
+import logo_icon from './logoIcon.png'
+import logo_big from './logoBig.png'
+import logo from './logo.png'
+import search_icon from './search_icon.png'
+import send_button from './send_button.png'
+import menu_icon from './menu_icon.png'
+import arrow_icon from './arrow_icon.png'
+import code from './code.svg'
+import bgImage from './bg.jpg'
+
+
+const assets = {
+    avatar_icon,
+    gallery_icon,
+    help_icon,
+    logo_big,
+    logo_icon,
+    logo,
+    search_icon,
+    send_button,
+    menu_icon,
+    arrow_icon,
+    code,
+    bgImage,
+
+}
+
+export default assets;
+
